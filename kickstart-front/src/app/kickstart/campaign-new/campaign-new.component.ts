0154fc78287660b641fbe68f0c895695d7d4a9da
6ssou <6ssou@protonmail.com>
2022-06-20 14:16:45 +0200

    remove useless ngoninit  &   === instead of ==

--- kickstart-front/src/app/kickstart/campaign-new/campaign-new.component.ts
+++ kickstart-front/src/app/kickstart/campaign-new/campaign-new.component.ts
@@ -1,20 +1,18 @@
 import { Subscription } from 'rxjs';
 import { CampaignService } from './../../services/campaign.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-campaign-new',
   templateUrl: './campaign-new.component.html',
   styleUrls: ['./campaign-new.component.less'],
 })
-export class CampaignNewComponent implements OnInit {
+export class CampaignNewComponent {
   libelle: string = '';
   minimumContribution: number = 0;
   subscription: Subscription = new Subscription();
 
   constructor(private campaignService: CampaignService) {}
-
-  ngOnInit(): void {}
 
   onCreateNewCampaign() {
     this.subscription.add(
