fcc2e140c014eef64283f4884cc61d83af6f4877
6ssou <6ssou@protonmail.com>
2022-06-25 14:33:47 +0200

    test campaign list

--- kickstart-front/src/app/kickstart/campaign-list/campaign-list.component.spec.ts
+++ kickstart-front/src/app/kickstart/campaign-list/campaign-list.component.spec.ts
@@ -1,15 +1,22 @@
+import { MatCardModule } from '@angular/material/card';
 import { CampaignService } from 'src/app/services/campaign.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CampaignListComponent } from './campaign-list.component';
+import { from } from 'rxjs';
 
 describe('CampaignListComponent', () => {
   let component: CampaignListComponent;
   let fixture: ComponentFixture<CampaignListComponent>;
+  let service: Partial<CampaignService> = {
+    getDeployedCampaigns: () => {
+      return from([[{ libelle: 'test', campaignAddress: "0x1234567" }, { libelle: 'test2', campaignAddress: "0x1234567" }]]);
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CampaignListComponent]
+      declarations: [CampaignListComponent], imports: [MatCardModule]
     })
       .compileComponents();
   });
@@ -17,7 +24,7 @@ describe('CampaignListComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CampaignListComponent],
-      providers: [CampaignService]
+      providers: [{ provide: CampaignService, useValue: service }]
     }).compileComponents();
     fixture = TestBed.createComponent(CampaignListComponent);
     component = fixture.componentInstance;
@@ -27,4 +34,12 @@ describe('CampaignListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should display the campaigns", () => {
+    const element: HTMLElement = fixture.nativeElement;
+    fixture.detectChanges();
+    const cards = element.getElementsByTagName('mat-card');
+    expect(cards.length).toBe(2);
+  })
+
 });
